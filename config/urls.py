2fdc217c357661e63d7fe03fad5db1a6cebfdd50
kyeon06 <jkyeon06@gmail.com>
2023-11-19 15:43:27 +0900

    :truck: build : project setting

---
+++ config/urls.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Project Management App",
+        default_version="v1",
+        description="프로젝트 관리 및 분석 기능을 제공하는 서비스입니다.",
+        contact=openapi.Contact(email="jkyeon06@gmail.com"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    # admin
+    path('admin/', admin.site.urls),
+
+    # api
+
+    # swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
