09107a7da49af4ce56f33c5023569858d8f31c08
kyeon06 <111492415+kyeon06@users.noreply.github.com>
2023-11-25 01:05:02 +0900

    [기능 추가] 사용자 회원가입 및 로그인 API 개발 (#13)

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # api
+    path('api/v1/users/', include('users.urls')),
 
     # swagger
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
---
+++ users/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, username, password, **extra_fields):
+        if not username:
+            raise ValueError("username은 필수 입력 내용입니다.")
+        
+        if not email:
+            raise ValueError("email은 필수 입력 내용입니다.")
+        
+        user = self.model(
+            username=username,
+            email=email,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, username, password, **extra_fields):
+        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username=username, email=email, password=password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField("계정명", max_length=20, unique=True)
+    email = models.EmailField("계정이메일", unique=True, max_length=100)
+
+    is_active = models.BooleanField("활성화여부", default=True)
+    is_admin = models.BooleanField("관리자여부", default=False)
+    is_superuser = models.BooleanField("superuser 여부", default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        db_table = "users"
+
+    def __str__(self):
+        return self.username
+    
+    @property
+    def is_staff(self):
+        """ admin 권한 설정 """
+        return self.is_admin
+
