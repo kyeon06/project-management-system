27c1c5432b8f2c8a997a3ff243ceb4acbe8f36c2
kyeon06 <jkyeon06@gmail.com>
2023-11-27 17:38:42 +0900

    :sparkles: feat : 팀원 초대 API 구현

--- teams/urls.py
+++ teams/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from teams.views import TeamCreateAPIView
+from teams.views import TeamCreateAPIView, TeamInviteAPIView
 
 urlpatterns = [
     path('', TeamCreateAPIView.as_view(), name='team-create'),
+    path('<int:team_id>/invite/', TeamInviteAPIView.as_view(), name='team-invite'),
 ]
--- users/serializers.py
+++ users/serializers.py
@@ -3,6 +3,18 @@ from rest_framework import serializers
 from users.models import User
 
 
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'email',
+            'invite_message'
+        ]
+
+
 class SignupSerializer(serializers.ModelSerializer):
 
     class Meta:
